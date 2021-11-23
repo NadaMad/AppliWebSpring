8d819a86844ea72690551c586e279c18aaa7a682
NadaMad <44082910+NadaMad@users.noreply.github.com>
2021-11-23 11:23:13 +0100

    Delete frontFilms directory

--- frontFilms/src/Main.java
+++
@@ -1,9 +0,0 @@
-import swingClass.MainApplication;
-
-public class Main {
-    public static void main(String[] args) {
-
-       MainApplication mainApplication = new MainApplication();
-    }
-
-}
